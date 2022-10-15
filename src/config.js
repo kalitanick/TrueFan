33754fb775ff2c09f5c3d473318c0ce2b07e4163
Nick <kalitanick@gmail.com>
2022-10-14 20:57:46 -0500

    Fixing environment variables to use REACT_APP_

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 //Constants for Spotify Authentication Calls
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = process.env.CLIENT_ID;
-export const redirectUri = "https://truefanplaylist.com/redirect#";
+export const clientId = process.env.REACT_APP_CLIENT_ID;
+export const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 //Spotify user scopes to create and modify playlists
 export const scopes = [
     "playlist-modify-private",
